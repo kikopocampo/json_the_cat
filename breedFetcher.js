036ede0acf91608804dcf6771e2a168362dba87b
Francisco <kikopocampo@gmail.com>
2022-10-27 14:48:39 -0600

    refactored and exported function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,16 @@
 const request = require('request');
-const argv = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv}`,(error,response,body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const catInfo = JSON.parse(body);
-  if (catInfo.length === 0) {
-    console.log("BREED NOT FOUND");
-    return;
-  }
-  console.log(catInfo[0].description);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error,response,body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const catInfo = JSON.parse(body);
+    if (catInfo.length === 0) {
+      return callback("BREED NOT FOUND");
+    }
+    callback(null, catInfo[0].description);
+  });
+};
+
+module.exports = {fetchBreedDescription};
